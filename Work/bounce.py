262ca7d133b3a33bffa68631e51dd75731f6caf9
lena <srgv.lena@gmail.com>
2020-08-08 19:30:18 +0300

    tasks: bounce, mortgage

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
+# #!/usr/bin/env python
 # bounce.py
 #
 # Exercise 1.5
+
+height_curr = 100.0
+bounce_counter = 0
+
+while bounce_counter<10:
+    bounce_counter+=1
+    height_curr = 3*height_curr/5
+    print(bounce_counter, height_curr, round(height_curr,4))
+
+##
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
+# #!/usr/bin/env python
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment_0 = 2684.11
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+total_paid = 0.0
+month_counter = 0
+
+
+while principal > 0:
+    month_counter +=1
+    if (month_counter>=extra_payment_start_month) and (month_counter<=extra_payment_end_month):
+        payment = payment_0 + extra_payment
+    else:
+        payment = payment_0
+    if ((principal * (1+rate/12)) <= payment):
+        payment = principal * (1+rate/12)
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print(month_counter, total_paid, principal)
+    
+print('Total paid', total_paid)
+print('month count', month_counter)
+
+
