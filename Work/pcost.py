eee27da336747a8cbc0b25f69e5ac228baf87896
lena <srgv.lena@gmail.com>
2020-08-10 14:48:30 +0300

    ex 1/28 done (pcoast.py)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
+# #!/usr/bin/env python
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f).split(',')
+    print(headers)
+    for line in f:
+        row = line.split(',')
+        print(row)
+        total_cost+=int(row[1])*float(row[2])
+##
+print(total_cost)
